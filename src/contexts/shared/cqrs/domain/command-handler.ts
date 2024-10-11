7515eb7a4abdccabeca1f8726a78cf7a8fa93d5a
Javier Parada <javierparada@pm.me>
2024-10-11 13:23:14 +0200

    add command bus

--- src/contexts/shared/cqrs/domain/command-handler.ts
+++ src/contexts/shared/cqrs/domain/command-handler.ts
@@ -1,9 +1,9 @@
 import type { Command } from "./command";
 import type { CommandHandlerResponse } from "./command-handler-responde";
 
-interface CommandHandler<TCommand extends Command> {
+interface CommandHandler {
 
-    handle(command: TCommand): Promise<CommandHandlerResponse>;
+    handle(command: Command): Promise<CommandHandlerResponse>;
 
     get type(): string;
 
