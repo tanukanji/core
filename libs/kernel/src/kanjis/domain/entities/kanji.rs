a970be2739403f0fc2a832d47d694902892b3835
Javier Parada <javierparada@pm.me>
2024-11-29 19:02:26 +0100

    Add Aggregate root impl

--- libs/kernel/src/kanjis/domain/entities/kanji.rs
+++ libs/kernel/src/kanjis/domain/entities/kanji.rs
@@ -1,3 +1,5 @@
+use aggregate_root::domain::aggregate_root::AggregateRoot;
+
 use super::{kanji_ideogram::KanjiIdeogram, kanji_kunyomi_reading::KanjiKunyomiReading, kanji_meaning::KanjiMeaning, kanji_nanori_reading::KanjiNanoriReading, kanji_onyomi_reading::KanjiOnyomiReading, kanji_radical::KanjiRadical, kanji_strokes_number::KanjiStrokesNumber};
 
 
@@ -9,4 +11,23 @@ pub struct Kanji {
     pub meanings: Vec<KanjiMeaning>,
     pub strokes: KanjiStrokesNumber,
     pub radicals: Vec<KanjiRadical>,
+}
+
+impl Kanji {
+    pub fn new(ideogram: KanjiIdeogram, kunyomi_readings: Vec<KanjiKunyomiReading>, onyomi_readings: Vec<KanjiOnyomiReading>, 
+        nanori_readings: Vec<KanjiNanoriReading>, meanings: Vec<KanjiMeaning>, strokes: KanjiStrokesNumber, radicals: Vec<KanjiRadical>) -> Self {
+        Self { ideogram, kunyomi_readings, onyomi_readings, nanori_readings, meanings, strokes, radicals }
+    }
+}
+
+impl AggregateRoot for Kanji {
+    fn get_type() -> String {
+        "tanukanji.kernel.kanji".to_string()
+    }
+}
+
+impl Clone for Kanji {
+    fn clone(&self) -> Self {
+        Self::new(self.ideogram.clone(), self.kunyomi_readings.clone(), self.onyomi_readings.clone(), self.nanori_readings.clone(), self.meanings.clone(), self.strokes.clone(), self.radicals.clone())
+    }
 }
